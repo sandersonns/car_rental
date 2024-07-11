e000774a3c6f4da93d19e90f1d366fbd77bca8c3
Hai Dinh NGUYEN <sandersonns@gmail.com>
2024-07-11 16:31:28 +0200

    fixed various bugs with CRUD

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :brand, :model, :year_of_production, :address, :price_per_day, presence: true
 end
